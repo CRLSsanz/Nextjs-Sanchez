b7a513be1f1cc5fe24bfccc5998a2de1d88ae23e
Carlos Sanz <carlonchosanz@gmail.com>
2024-02-11 17:03:31 +0100

    24feb11 primera pagina computer setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,211 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gray-100 text-gray-600">
+      <section className="w-full fixed bottom-0">
+        <div className="w-full h-16 bg-gray-900/85 text-teal-400 flex justify-between items-center px-4">
+          <div className="">A</div>
+          <div className=""> = </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="mb-20 relative">
+        <div>
+          <img src="/images/fondo1.jpg" alt="fondo" className="" />
+        </div>
+        <div className="absolute top-0 w-full">
+          <div className="w-full bg-gray-900/10 text-white flex justify-between p-4">
+            <div>CRL</div>
+            <div>Login</div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="w-full h-full absolute top-0 bg-gray-900/ px-6">
+          <div className="h-full py-10 text-gray-200 flex flex-col justify-between leading-normal ">
+            <h1 className="w-full mt-20 uppercase text-2xl">
+              Computer Setup <br />G<span className="text-teal-300">a</span>ming
+              Room.
+            </h1>
+            <h1 className="text-xl text-end">
+              El color blanco es <br />
+              mejor cuando se mezcla <br /> con el minimalismo
+            </h1>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/** PRODUCTS MONITOR  */}
+      <section className="mb-6">
+        <h1 className="p-6 uppercase">Monitor HD</h1>
+        <div className="w-full flex overflow-x-scroll">
+          <div className="pl-6">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-60 flex items-center">
+                <img src="/images/monitor-uw-curvo.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  ACER Predator Z35P Nvidia G-Sync 21:9
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Dimenciones: 34" 29" 27" 25"</h1>
+                  <h1 className="">Resolucion: UHD QHD 2K 4K</h1>
+                  <h1 className="">Marca: ACER ASUS HP LG SAMSUMG</h1>
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-60 flex items-center">
+                <img src="/images/monitor-uw.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  ASUS ROG Swift PG-8Q Nvidia G-Sync 21:9 Republic of Gamers
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Dimenciones: 34" 29" 27" 25"</h1>
+                  <h1 className="">Resolucion: UHD QHD 2K 4K</h1>
+                  <h1 className="">Marca: ACER ASUS HP LG SAMSUMG</h1>
+                </div>
+              </div>
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-60 flex items-center">
+                <img src="/images/monitor-hd.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  Dell, U2518D UltraSharp Monitor (Black) PC Computer LED HD
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Dimenciones: 27" 25" 22" 19"</h1>
+                  <h1 className="">Resolucion: HD UHD 2K </h1>
+                  <h1 className="">Marca: ACER DELL HP LG SAMSUMG</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/** PRODUCTS RAM  */}
+      <section className="mb-6">
+        <h1 className="p-6 uppercase">Memoria RAM</h1>
+        <div className="w-full flex overflow-x-scroll">
+          <div className="pl-6">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-48 flex items-center">
+                <img src="/images/ram-ddr5.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  DDR5 SDRAM Computer data storage G.Skill DIMM, sunshine light
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Tecnologia : DDR5 SDRAM</h1>
+                  <h1 className="">Dimenciones : 16GB 32GB</h1>
+                  <h1 className="">Transferencia : 4800Mhz 5600Mhz</h1>
+                  <div className="flex flex-row">
+                    <h1 className="mr-1 inline text-nowrap">Marca :</h1>
+                    <h1 className="uppercase">
+                      Corsair Crucial Kingston Lexar Patriot
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-48 flex items-center">
+                <img src="/images/ram-ddr4.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  Black Fury Hyper DDR4 RAM stick, memory Kingston Technology
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Tecnologia : DDR4 SDRAM</h1>
+                  <h1 className="">Dimenciones : 8GB 16GB</h1>
+                  <h1 className="">Transferencia : 2400 3200 3600</h1>
+                  <div className="flex flex-row">
+                    <h1 className="mr-1 inline text-nowrap">Marca :</h1>
+                    <h1 className="uppercase">
+                      Corsair Gigastone Kingston Lexar Patriot
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-48 flex items-center">
+                <img src="/images/ram-ddr3.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  DDR3 SDRAM Computer data storage Kingston Technology DIMM
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Tecnologia : DDR3 SDRAM</h1>
+                  <h1 className="">Dimenciones : 4GB 8GB</h1>
+                  <h1 className="">Transferencia : 1333Mhz 1600Mhz </h1>
+                  <div className="flex flex-row">
+                    <h1 className="mr-1 inline text-nowrap">Marca :</h1>
+                    <h1 className="uppercase">
+                      Corsair Gigastone Kingston Patriot
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="h-full p-4 bg-white rounded mr-4">
+              <div className="w-60 h-48 flex items-center">
+                <img src="/images/ram-laptop.png" alt="" className="" />
+              </div>
+              <div className="">
+                <div className="text-center mb-2">
+                  Laptop SO-DIMM DDR4 SDRAM, Laptop, electronics, transfer, ram
+                  png
+                </div>
+                <div className="text-sm text-gray-400">
+                  <h1 className="">Tecnologia : DDR3 DDR4 SDRAM</h1>
+                  <h1 className="">Dimenciones : 4GB 8GB 16GB</h1>
+                  <h1 className="">Transferencia : 1600 - 2400 - 3200 </h1>
+                  <div className="flex flex-row">
+                    <h1 className="mr-1 inline text-nowrap">Marca :</h1>
+                    <h1 className="uppercase">
+                      Corsair Crucial Kingston Patriot
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <div className="mb-20">
+            <img src="/images/ima1.png" alt="fondo" />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
