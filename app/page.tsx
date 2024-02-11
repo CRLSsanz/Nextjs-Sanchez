083cd4b485226dadfa5699daed11c1ff8d84670f
Carlos Sanz <carlonchosanz@gmail.com>
2024-02-11 18:12:08 +0100

    se agrego el fonfo

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,19 @@ export default function page() {
     <div className="bg-gray-100 text-gray-600">
       <section className="w-full fixed bottom-0">
         <div className="w-full h-16 bg-gray-900/85 text-teal-400 flex justify-between items-center px-4">
-          <div className="">A</div>
-          <div className=""> = </div>
+          <div>A</div>
+          <div> = </div>
         </div>
       </section>
 
       <section className="mb-20 relative">
         <div>
-          <Image src="/images/fondo1.jpg" width={1000} height={1500} alt="" />
+          <Image
+            src="/images/fondo1.jpg"
+            width={1000}
+            height={1500}
+            alt="images"
+          />
         </div>
         <div className="absolute top-0 w-full">
           <div className="w-full bg-gray-900/10 text-white flex justify-between p-4">
@@ -47,63 +52,63 @@ export default function page() {
                   src="/images/monitor-uw-curvo.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   ACER Predator Z35P Nvidia G-Sync 21:9
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Dimenciones: 34" 29" 27" 25"</h1>
-                  <h1 className="">Resolucion: UHD QHD 2K 4K</h1>
-                  <h1 className="">Marca: ACER ASUS HP LG SAMSUMG</h1>
+                  <h1>Dimenciones: 34" 29" 27" 25"</h1>
+                  <h1>Resolucion: UHD QHD 2K 4K</h1>
+                  <h1>Marca: ACER ASUS HP LG SAMSUMG</h1>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="">
+          <div>
             <div className="h-full p-4 bg-white rounded mr-4">
               <div className="w-60 h-60 flex items-center">
                 <Image
                   src="/images/monitor-uw.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   ASUS ROG Swift PG-8Q Nvidia G-Sync 21:9 Republic of Gamers
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Dimenciones: 34" 29" 27" 25"</h1>
-                  <h1 className="">Resolucion: UHD QHD 2K 4K</h1>
-                  <h1 className="">Marca: ACER ASUS HP LG SAMSUMG</h1>
+                  <h1>Dimenciones: 34" 29" 27" 25"</h1>
+                  <h1>Resolucion: UHD QHD 2K 4K</h1>
+                  <h1>Marca: ACER ASUS HP LG SAMSUMG</h1>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="">
+          <div>
             <div className="h-full p-4 bg-white rounded mr-4">
               <div className="w-60 h-60 flex items-center">
                 <Image
                   src="/images/monitor-hd.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   Dell, U2518D UltraSharp Monitor (Black) PC Computer LED HD
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Dimenciones: 27" 25" 22" 19"</h1>
-                  <h1 className="">Resolucion: HD UHD 2K </h1>
-                  <h1 className="">Marca: ACER DELL HP LG SAMSUMG</h1>
+                  <h1>Dimenciones: 27" 25" 22" 19"</h1>
+                  <h1>Resolucion: HD UHD 2K </h1>
+                  <h1>Marca: ACER DELL HP LG SAMSUMG</h1>
                 </div>
               </div>
             </div>
@@ -122,17 +127,17 @@ export default function page() {
                   src="/images/ram-ddr5.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   DDR5 SDRAM Computer data storage G.Skill DIMM, sunshine light
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Tecnologia : DDR5 SDRAM</h1>
-                  <h1 className="">Dimenciones : 16GB 32GB</h1>
-                  <h1 className="">Transferencia : 4800Mhz 5600Mhz</h1>
+                  <h1>Tecnologia : DDR5 SDRAM</h1>
+                  <h1>Dimenciones : 16GB 32GB</h1>
+                  <h1>Transferencia : 4800Mhz 5600Mhz</h1>
                   <div className="flex flex-row">
                     <h1 className="mr-1 inline text-nowrap">Marca :</h1>
                     <h1 className="uppercase">
@@ -144,24 +149,24 @@ export default function page() {
             </div>
           </div>
 
-          <div className="">
+          <div>
             <div className="h-full p-4 bg-white rounded mr-4">
               <div className="w-60 h-48 flex items-center">
                 <Image
                   src="/images/ram-ddr4.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   Black Fury Hyper DDR4 RAM stick, memory Kingston Technology
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Tecnologia : DDR4 SDRAM</h1>
-                  <h1 className="">Dimenciones : 8GB 16GB</h1>
-                  <h1 className="">Transferencia : 2400 3200 3600</h1>
+                  <h1>Tecnologia : DDR4 SDRAM</h1>
+                  <h1>Dimenciones : 8GB 16GB</h1>
+                  <h1>Transferencia : 2400 3200 3600</h1>
                   <div className="flex flex-row">
                     <h1 className="mr-1 inline text-nowrap">Marca :</h1>
                     <h1 className="uppercase">
@@ -173,24 +178,24 @@ export default function page() {
             </div>
           </div>
 
-          <div className="">
+          <div>
             <div className="h-full p-4 bg-white rounded mr-4">
               <div className="w-60 h-48 flex items-center">
                 <Image
                   src="/images/ram-ddr3.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   DDR3 SDRAM Computer data storage Kingston Technology DIMM
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Tecnologia : DDR3 SDRAM</h1>
-                  <h1 className="">Dimenciones : 4GB 8GB</h1>
-                  <h1 className="">Transferencia : 1333Mhz 1600Mhz </h1>
+                  <h1>Tecnologia : DDR3 SDRAM</h1>
+                  <h1>Dimenciones : 4GB 8GB</h1>
+                  <h1>Transferencia : 1333Mhz 1600Mhz </h1>
                   <div className="flex flex-row">
                     <h1 className="mr-1 inline text-nowrap">Marca :</h1>
                     <h1 className="uppercase">
@@ -202,25 +207,25 @@ export default function page() {
             </div>
           </div>
 
-          <div className="">
+          <div>
             <div className="h-full p-4 bg-white rounded mr-4">
               <div className="w-60 h-48 flex items-center">
                 <Image
                   src="/images/ram-laptop.png"
                   width={1000}
                   height={1000}
-                  alt=""
+                  alt="images"
                 />
               </div>
-              <div className="">
+              <div>
                 <div className="text-center mb-2">
                   Laptop SO-DIMM DDR4 SDRAM, Laptop, electronics, transfer, ram
                   png
                 </div>
                 <div className="text-sm text-gray-400">
-                  <h1 className="">Tecnologia : DDR3 DDR4 SDRAM</h1>
-                  <h1 className="">Dimenciones : 4GB 8GB 16GB</h1>
-                  <h1 className="">Transferencia : 1600 - 2400 - 3200 </h1>
+                  <h1>Tecnologia : DDR3 DDR4 SDRAM</h1>
+                  <h1>Dimenciones : 4GB 8GB 16GB</h1>
+                  <h1>Transferencia : 1600 - 2400 - 3200 </h1>
                   <div className="flex flex-row">
                     <h1 className="mr-1 inline text-nowrap">Marca :</h1>
                     <h1 className="uppercase">
@@ -237,7 +242,12 @@ export default function page() {
       <section>
         <div>
           <div className="mb-20">
-            <Image src="/images/ima1.png" width={1000} height={1000} alt="" />
+            <Image
+              src="/images/ima1.png"
+              width={1000}
+              height={1000}
+              alt="images"
+            />
           </div>
         </div>
       </section>
